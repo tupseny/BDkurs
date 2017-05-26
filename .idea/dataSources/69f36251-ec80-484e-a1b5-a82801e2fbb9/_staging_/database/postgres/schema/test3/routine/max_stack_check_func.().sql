3359ddcfceca5ce341f79970738ebc05194f7d20
Martin <marsik98@gmail.com>
2017-05-26 06:49:29 +0300

    //fixed - bug with triggers: max_stack_check

--- .idea/dataSources/69f36251-ec80-484e-a1b5-a82801e2fbb9/_staging_/database/postgres/schema/test3/routine/max_stack_check_func.().sql
+++ .idea/dataSources/69f36251-ec80-484e-a1b5-a82801e2fbb9/_staging_/database/postgres/schema/test3/routine/max_stack_check_func.().sql
@@ -1,8 +1,8 @@
 create function test3.max_stack_check_func() RETURN "trigger"
 AS $$
 begin
-  IF new.count > (select max_stack from item full join inventory
-  where new.item_id = item.ID)
+  IF (new.count > (select max_stack from item full join inventory
+  where new.item_id = item.ID))
   then
     return NULL;
   else
